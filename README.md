82c6cf53beca8c57a652fdabfb9a455995ed426b
spencermeyer <spencer_meyer@hotmail.com>
2015-09-19 20:06:03 +0100

    added gpio routines into the toggle next to test on Pi

--- README.md
+++ README.md
@@ -4,12 +4,17 @@ Intending to use the pi-gpio javascript library
 
 Basing the App on Node.js and I am including all the dependencies (like boostrap, JQuery etc) locally as the pi will not necessarily have internet access when I am using the "website" to control the Pi's IO.
 
+I am particularly pleased with the loop that adds any number of event listeners to the code, this is DRY programming!  Is it meta programming?  The code is generating it's own methods.
+
+There are some overrides in my CSS file.  For some reason my Bootsrtap.css file did not contain the class btn-lg(?!), so I wrote it.
+
+INTRUCTIONS:
+Uncomment GPIO in index.js and in Actions.js before deploying on the PI (don't try to use them on a non Pi!)
+
 go to here:
 https://www.npmjs.com/package/pi-gpio
 to get the GPIO JS Library
 
-
 good source for how to use it:
 http://www.robert-drummond.com/2013/06/06/raspberry-pi-gpio-inputoutput-in-javascript/
-
 
--- public/actions.js
+++ public/actions.js
@@ -9,16 +9,23 @@ $( document ).ready(function() {
       return function(){
         console.log("a call to getClickFunction"+j);
         console.log("button "+j+" clicked");
+        var gpioPin = j+15;
         // Now toggle the state of the output when clicked
         if(document.getElementById("btn"+String(j)).innerHTML=="Channel "+j+" OFF"){
           console.log("going into the switch on loop for channel "+j);
           document.getElementById("btn"+j).innerHTML="Channel "+j+" ON";
           document.getElementById("btn"+j).className="btn btn-success btn-lg";
+          // gpio.open(gpioPin, "output", function(err){
+          //  gpio.write(gpioPin,1,function(){
+          // gpio.close(gpioPin);  
+          //})  
+          //})
         }
         else{
           console.log("going into the switch off loop");
           document.getElementById("btn"+j).innerHTML="Channel "+j+" OFF";
           document.getElementById("btn"+j).className="btn btn-danger btn-lg";
+          //gpio.close(gpioPin);
         }
     }
 }
