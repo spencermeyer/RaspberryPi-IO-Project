a9c1aedccf794e10e71486a60e04fa408d76eb86
spencermeyer <spencer_meyer@hotmail.com>
2015-09-19 15:48:59 +0100

    tidied up and removed for loop

--- public/actions.js
+++ public/actions.js
@@ -1,30 +1,23 @@
 $( document ).ready(function() {
-  console.log( "ready!" );
+
+  // for (i=1; i<3.5; i++) {
 
   document.getElementById("btn1").addEventListener("click", function(){
-    console.log("button 1 clicked");
-    //gpio.open(16, "output", function(err) {     // Open pin 16 for output 
-    //    gpio.write(16, 1, function() {          // Set pin 16 high (1) 
-    //        gpio.close(16);                     // Close pin 16 
-    //    });
-    //});
-  // first find the current state of the output
-  var stateOne = document.getElementById("btn1").innerHTML
-  // Now toggle the state of the output when clicked
-  if(stateOne=="Channel 1 OFF"){
-    console.log(stateOne);
-    document.getElementById("btn1").innerHTML="Channel 1 ON";
-    document.getElementById("btn1").className="btn btn-success btn-lg";
-    stateOne="Channel 1 ON";
-    console.log(stateOne);
-  }
-  else{
-    document.getElementById("btn1").innerHTML="Channel 1 OFF";
-    document.getElementById("btn1").className="btn btn-danger btn-lg";
-  }
+      console.log("button 1 clicked");
 
-  
+  // Now toggle the state of the output when clicked
+    if(document.getElementById("btn1").innerHTML=="Channel 1 OFF"){
+      console.log("going into the switch on loop");
+      document.getElementById("btn1").innerHTML="Channel 1 ON";
+      document.getElementById("btn1").className="btn btn-success btn-lg";
+    }
+    else{
+      console.log("going into the switch off loop");
+      document.getElementById("btn1").innerHTML="Channel 1 OFF";
+      document.getElementById("btn1").className="btn btn-danger btn-lg";
+    }
 });
 
+//the for loop}
 
 });
---
+++ public/to_implement.js
@@ -0,0 +1,5 @@
+//gpio.open(16, "output", function(err) {     // Open pin 16 for output 
+//    gpio.write(16, 1, function() {          // Set pin 16 high (1) 
+//        gpio.close(16);                     // Close pin 16 
+//    });
+//});
